4b7c1b86b1df38f4a3f72a49ea8d83bd0a7605b2
Paulo Sergio <pauloserg861@gmail.com>
2024-09-29 15:40:52 -0300

    refactor: move imports to a single file

--- src/presentation/protocols/controller.ts
+++ src/presentation/protocols/controller.ts
@@ -1,4 +1,4 @@
-import { HttpRequest, HttpResponse } from './http-request';
+import { HttpRequest, HttpResponse } from './http';
 
 export interface Controller {
   handle(httpRequest: HttpRequest): HttpResponse;
