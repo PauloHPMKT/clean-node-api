7214489ff5a89e55bc39bbecca299ec4da8df33a
Paulo Sergio <pauloserg861@gmail.com>
2024-09-29 15:57:11 -0300

    test: ensure SignUpController returns 400 if passwordConfirmation fails

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -68,7 +68,7 @@ describe('SignUp Controller', () => {
     expect(httpResponse.body).toEqual(new MissingParamError('password'));
   });
 
-  it('Should return 400 if no pconfirmationPassword is provided', () => {
+  it('Should return 400 if no confirmationPassword is provided', () => {
     const { sut } = makeSut();
     const httpRequest = {
       body: {
@@ -81,6 +81,23 @@ describe('SignUp Controller', () => {
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual(
       new MissingParamError('passwordConfirmation'),
+    );
+  });
+
+  it('Should return 400 if passwordConfirmation fails', () => {
+    const { sut } = makeSut();
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'invalid_password',
+      },
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(
+      new InvalidParamError('passwordConfirmation'),
     );
   });
 
