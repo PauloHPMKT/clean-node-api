29317368b0f4487e5004f62d075ac598389f1733
Paulo Sergio <pauloserg861@gmail.com>
2024-12-13 15:51:49 -0300

    feat: ensure BcryptAdapter calls correct values

---
+++ src/infra/criptography/bcrypt-adapter.ts
@@ -0,0 +1,16 @@
+import { Encrypter } from '../../data/protocols/encrypter';
+import bcrypt from 'bcrypt';
+
+export class BcryptAdapter implements Encrypter {
+  private readonly salt: number;
+  // Salt é adicionado no construtor para que a classe BcryptAdapter seja responsável por definir o salt
+  // e não a implementação do protocolo Encrypter
+  constructor(salt: number) {
+    this.salt = salt;
+  }
+
+  async encrypt(value: string): Promise<string> {
+    await bcrypt.hash(value, this.salt);
+    return '';
+  }
+}
