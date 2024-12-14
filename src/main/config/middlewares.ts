3a3d5645480bcb0dd7ecee9f3628e9577925556d
Paulo Sergio <pauloserg861@gmail.com>
2024-12-14 16:51:09 -0300

    feat: ensure request set cors enabled

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,7 +1,8 @@
 import { Express } from 'express';
 import { bodyParser } from '../middlewares/body-parser';
+import { cors } from '../middlewares/cors';
 
 export default (app: Express): void => {
-  const middlewares = [bodyParser];
+  const middlewares = [bodyParser, cors];
   middlewares.forEach((middleware) => app.use(middleware));
 };
