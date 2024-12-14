135bdc3dacf7dd1bd0b5051dc318d678e31321d4
Paulo Sergio <pauloserg861@gmail.com>
2024-12-14 16:59:30 -0300

    feat: ensure request return json as default content-type

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,8 +1,9 @@
 import { Express } from 'express';
 import { bodyParser } from '../middlewares/body-parser';
 import { cors } from '../middlewares/cors';
+import { contentType } from '../middlewares/content-type';
 
 export default (app: Express): void => {
-  const middlewares = [bodyParser, cors];
+  const middlewares = [bodyParser, cors, contentType];
   middlewares.forEach((middleware) => app.use(middleware));
 };
